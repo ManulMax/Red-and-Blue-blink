498290633eed21a39d6904ff563538b45c2afb67
Manul Madara <49731724+ManulMax@users.noreply.github.com>
2019-09-14 14:52:13 +0530

    Add files via upload

---
+++ ledR_B.ino
@@ -0,0 +1,17 @@
+int ledR=2; // red led to the 2nd digital pin
+int ledB=3; // blue led to the 3rd digi pin
+void setup() {
+  pinMode(ledR, OUTPUT);
+  pinMode(ledB, OUTPUT);
+}
+
+void loop() {
+  //on red led for 100 miliseconds
+  digitalWrite(ledR, HIGH);
+  delay(100);
+  digitalWrite(ledR ,LOW);
+  //on blue led for another 100 miliseconds
+  digitalWrite(ledB, HIGH);
+  delay(100);
+  digitalWrite(ledB ,LOW);
+}
